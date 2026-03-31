d09d91401138d5133f23b1afca7336f49b79ff35
LinCie <aldiofernanda@protonmail.com>
2026-03-31 10:59:38 +0000

    feat: fix bundle size error

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
@@ -14,7 +15,38 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    visualizer({
+      filename: 'dist/client/stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
+  environments: {
+    client: {
+      build: {
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                if (
+                  id.includes('better-auth') ||
+                  id.includes('@better-auth')
+                ) {
+                  return 'vendor-auth'
+                }
+                if (id.includes('@dnd-kit')) {
+                  return 'vendor-dnd'
+                }
+                if (id.includes('browser-image-compression')) {
+                  return 'vendor-image-compression'
+                }
+              }
+            },
+          },
+        },
+      },
+    },
+  },
 })
 
 export default config
